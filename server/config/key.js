20f14739a2bfae659b869412e193dcc02a333be0
ezeun <consistency1231@gmail.com>
2022-02-13 14:11:12 +0900

    #3 비디오 업로드 Form 만들기 1

--- server/config/key.js
+++ server/config/key.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
+    module.exports = require('./prod'); //produnction mode에서 개발 시
 } else {
-    module.exports = require('./dev');
+    module.exports = require('./dev'); //localhost에서 개발 시
 }
