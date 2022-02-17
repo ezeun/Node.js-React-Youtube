5dfd5633bae1eb0430be9fe0edeb83e76590498c
ezeun <consistency1231@gmail.com>
2022-02-17 11:53:01 +0900

    #11 구독 기능 1

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
 import SideVideo from './Sections/SideVideo';
+import Subscribe from './Sections/Subscribe';
 
 function VideoDetailPage(props) {
 
@@ -32,7 +33,7 @@ function VideoDetailPage(props) {
                         <video style={{width:'100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
     
                         <List.Item
-                            actions
+                            actions={[<Subscribe userTo={VideoDetail.writer._id } />]} //Subscribe.js 참고
                         >
                             <List.Item.Meta
                                 avatar={<Avatar src={VideoDetail.writer.image} />}
@@ -47,7 +48,7 @@ function VideoDetailPage(props) {
                 </Col>
 
                 <Col lg={6} xs={24}>
-                    <SideVideo />
+                    <SideVideo /> 
                 </Col>
             </Row>
         )
